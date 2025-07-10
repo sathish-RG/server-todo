d359ead64cd4b7ea86b6e0506fdedc1b2582b8e2
sathish-RG <sathish958527@gmail.com>
2025-07-10 17:30:58 +0530

    live-URL

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(morgan('dev'));
 app.use(cors({
-  origin:'http://localhost:5173',
+  origin:'https://totofull.netlify.app/',
   credentials:true,
 }));
 
